cb0cd48070d776c06b634075727c6e57585c91cb
Sophie Stelmach <sophiestelmach@yahoo.ca>
2023-09-07 13:46:15 -0400

    kinda works??

--- AARMS_Lyme_Shiny.R
+++ AARMS_Lyme_Shiny.R
@@ -22,47 +22,118 @@ ui <- fluidPage(
                         label ="Proportion of rodents with acaricide treatment",
                         min = 0.0,
                         max = 0.999,
-                        value = 0.2)
+                        value = 0.2),
+            sliderInput(inputId = "b_l",
+                        label ="Rate at which larval ticks attach on rodents",
+                        min = 0.0,
+                        max = 0.999,
+                        value = 0.5),
+            sliderInput(inputId = "b_n",
+                        label ="Rate at which nymphal ticks bite rodents",
+                        min = 0.0,
+                        max = 0.999,
+                        value = 0.5),
+            sliderInput(inputId = "r",
+                        label ="Proportion of rodents with repellent insecticide",
+                        min = 0.0,
+                        max = 0.999,
+                        value = 0),
+            sliderInput(inputId = "delta",
+                        label ="Probability of co-feeding transmission",
+                        min = 0.0,
+                        max = 0.999,
+                        value = 0.7),
+            sliderInput(inputId = "mu_l",
+                        label ="Death rate of larval ticks",
+                        min = 0.0,
+                        max = 0.999,
+                        value = 0.01),
+            sliderInput(inputId = "mu_n",
+                        label ="Death rate of nymphal ticks",
+                        min = 0.0,
+                        max = 0.999,
+                        value = 0.002),
+            sliderInput(inputId = "mu_a",
+                        label ="Death rate of adult ticks",
+                        min = 0.0,
+                        max = 0.999,
+                        value = 0.1),
+            sliderInput(inputId = "mu_h",
+                        label ="Death rate of rodents",
+                        min = 0.0,
+                        max = 0.999,
+                        value = 0.001),
+            sliderInput(inputId = "beta",
+                        label ="Maximum birth rate for larvae",
+                        min = 0.0,
+                        max = 50,
+                        value = 15),
+            sliderInput(inputId = "gamma",
+                        label ="Intensity of density-dependence in larvae birth rate",
+                        min = 0.0,
+                        max = 0.999,
+                        value = 0.0005),
+            sliderInput(inputId = "gamma_h",
+                        label ="Recovery rate of hosts",
+                        min = 0.0,
+                        max = 0.999,
+                        value = 0.1),
+            sliderInput(inputId = "p_hl",
+                        label ="Probability of transmission from host to larva",
+                        min = 0.0,
+                        max = 0.999,
+                        value = 0.8),
+            sliderInput(inputId = "p_nh",
+                        label ="Probability of transmission from nymph to host",
+                        min = 0.0,
+                        max = 0.999,
+                        value = 0.9),
+            sliderInput(inputId = "p_hn",
+                        label ="Probability of transmission from host to nymph",
+                        min = 0.0,
+                        max = 0.999,
+                        value = 0.8)
             
         ),
 
         # Show a plot of the generated distribution
-        mainPanel(
-           #plotOutput("plot")
-           textOutput("selected_var")
+        mainPanel(plotOutput("plot")
         )
+           #textOutput("selected_var")
+        
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    mod5 <- function(t, y , parms){
+    mod5 <- function(t, vars , parms){
+      with(as.list(c(vars, parms)), { 
         #Pull state variables from y vector
-        L = y[1]
-        Ns = y[2]
-        Ni = y[3]
-        A = y[4]
-        Hs = y[5]
-        Hi = y[6]
-        Hr = y[7]
+        # L = vars[1]
+        # Ns = vars[2]
+        # Ni = vars[3]
+        # A = vars[4]
+        # Hs = vars[5]
+        # Hi = vars[6]
+        # Hr = vars[7]
         
         #Pull the required parameter values from the parms vector
-        b_l=parms["b_l"]
-        b_n=parms["b_n"]
-        r=parms["r"]
-        a =parms["a"]
-        delta=parms["delta"]
-        mu_l=parms["mu_l"]
-        mu_n=parms["mu_n"]
-        mu_a=parms["mu_a"]
-        mu_h=parms["mu_h"]
-        beta=parms["beta"]
-        gamma=parms["gamma"]
-        gamma_h=parms["gamma_h"]
-        p_hl=parms["p_hl"]
-        p_nh=parms["p_nh"]
-        p_hn = parms["p_hn"]
+        # b_l=parms["b_l"]
+        # b_n=parms["b_n"]
+        # r=parms["r"]
+        # a =parms["a"]
+        # delta=parms["delta"]
+        # mu_l=parms["mu_l"]
+        # mu_n=parms["mu_n"]
+        # mu_a=parms["mu_a"]
+        # mu_h=parms["mu_h"]
+        # beta=parms["beta"]
+        # gamma=parms["gamma"]
+        # gamma_h=parms["gamma_h"]
+        # p_hl=parms["p_hl"]
+        # p_nh=parms["p_nh"]
+        # p_hn = parms["p_hn"]
         
         dL = beta*A*exp(-gamma*A) 
         - b_l*L*(1-r) - mu_l*L
@@ -93,43 +164,37 @@ server <- function(input, output) {
         dHr = gamma_h*Hi - mu_h*Hr
         
         res=c(dL, dNs, dNi, dA, dHs, dHi, dHr)
-        list(res)
+        return(list(res))
+      })
       }
     
     values <- reactive({
-      req(input$a)
-      times  = seq(0, 1000, by=1)
-      parms= c(
-        b_l=0.5,
-        b_n=0.5,
-        r=0,
-        delta=0.7,
-        mu_l=0.01,
-        mu_n=0.002,
-        mu_a=0.1,
-        mu_h=0.001,
-        beta=15,
-        gamma=0.0005,
-        gamma_h=0.1,
-        p_hl=0.8,
-        p_nh=0.9,
-        p_hn = 0.8
-      )
+      req(input$a, input$b_l, input$b_n, input$r, input$delta, input$mu_l,
+          input$mu_n, input$mu_a, input$mu_h, input$beta, input$gamma, 
+          input$gamma_h, input$p_hl, input$p_hn, input$p_nh)
+      
+      times  = seq(0, 5000, by=1)
+      parms= c(a = input$a, b_l = input$b_l, b_n = input$b_n, r = input$r, 
+               delta = input$delta, mu_l = input$mu_l,
+               mu_n = input$mu_n, mu_a = input$mu_a, mu_h = input$mu_h, 
+               beta = input$beta, gamma = input$gamma, 
+               gamma_h = input$gamma_h, p_hl = input$p_hl, 
+               p_hn = input$p_hn, p_nh = input$p_nh)
       start = c(L=6000, Ns=1000, Ni=1500, A=800, Hs=1000, Hi=2000, Hr=100)
-      out <- ode(y = start, times = times, func = mod5, 
-          parms = c(parms, a = input$a))
-      out = as.data.frame(out)
+      
+      ode(y = start, times = times, func = mod5, 
+          parms = parms)
+      
     })
     
-     output$selected_var <- renderText({ 
-       paste("You have selected", out[2,2])
-     })
     
     output$plot <- renderPlot({
+      out <- as.data.frame(values())
+      
         with(out, {
-        plot(x = out$time, y = out$L, col = "yellow", ylab = "Number of individuals",
-             xlab = "Time (days)", type = "l", xlim = c(0, 4000), ylim = c(-10,40000))
-        #lines(x = out$time, y = out$L, col = "yellow")
+        plot(x = time, y = L, col = "yellow", ylab = "Number of individuals",
+             xlab = "Time (days)", type = "l", xlim = c(0, 400), ylim = c(-10,4000))
+        lines(x = out$time, y = out$L, col = "yellow")
         lines(x = out$time, y = out$Ns, col = "red")
         lines(x = out$time, y = out$A, col = "purple")
         lines(x = out$time, y = out$Hi, col = "magenta")
